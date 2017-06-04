fe8616534b4e964e54adb0de0197b79c3c4ea04f
syma9 <ma.kevin2011@gmail.com>
2017-06-04 14:06:28 +0100

    initial

---
+++ js/buttons.js
@@ -0,0 +1,47 @@
+<script type ="text/javascript">
+
+document.getElementById("Heating").onclick = function() {
+	
+}
+
+document.getElementById("Illegal Parking").onclick = function() {
+	
+}
+document.getElementById("Blocked Driveway").onclick = function() {
+	
+}
+document.getElementById("Street Condition").onclick = function() {
+	
+}
+document.getElementById("Unsanitary Condition").onclick = function() {
+	
+}
+document.getElementById("Water System").onclick = function() {
+	
+}
+document.getElementById("Paint Condition").onclick = function() {
+	
+}
+document.getElementById("Street Light Condition").onclick = function() {
+	
+}
+document.getElementById("Plumbing").onclick = function() {
+	
+}
+document.getElementById("Allcomplaints").onclick = function() {
+	
+}
+
+document.getElementById("2015").onclick = function() {
+	document.getElementById("bar-highchart").html("src", 'http://cloud.highcharts.com/embed/yqepuku');
+}
+
+document.getElementById("2016").onclick = function() {
+	document.getElementById("bar-highchart").html("src", 'http://cloud.highcharts.com/embed/ahycuga');
+}
+
+document.getElementById("Allyears").onclick = function() {
+	
+}
+
+</script>
